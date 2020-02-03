fa27f209bc532c197244e19e71e1b71e53473074
Sarah Vens <sarah.vens@student.howest.be>
2020-02-03 21:15:07 +0100

    Opdracht JObstudent

--- 2d-Jobstudent.Wpf/MainWindow.xaml.cs
+++ 2d-Jobstudent.Wpf/MainWindow.xaml.cs
@@ -20,11 +20,58 @@ namespace _2d_Jobstudent.Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+ //Variabelen
+        float uurloon=14.45F;
+        Random rand = new Random();
+        int uren;
+
+//End Variabelen
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DateTime opstartTijd = DateTime.Now;
+            Title = Convert.ToString(opstartTijd);
+            lblUurloon.Content = uurloon;
+            txtNaam.Focus();
+            btnPlus.IsEnabled = false;
+            btnNaarLijst.Visibility = Visibility.Hidden;      
+        }
+
+        private void txtNaam_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            btnPlus.IsEnabled = true;
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            string naam;
+            naam = txtNaam.Text;
+            lstNamen.Items.Add(naam);
+            txtNaam.Text = "";
+            txtNaam.Focus();
+            btnPlus.IsEnabled = false;
+        }
+
+        private void lstNamen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            btnNaarLijst.Visibility = Visibility.Visible;
+            uren = rand.Next(1, 9);
+            lblUren.Content = Convert.ToString(uren);
+
+        }
+
+        private void btnNaarLijst_Click(object sender, RoutedEventArgs e)
+        {
+            string naam = lstNamen.SelectedItem.ToString();
+            string samenvatting;
+            samenvatting = $"{naam} - {uren} - {uurloon} ";
+            tbxLijst.Text += samenvatting + Environment.NewLine;
+            btnNaarLijst.Visibility = Visibility.Hidden;
+        }
     }
 }
